3adb8c2842bfc7478bfc4d3d84e26aac4aeb6445
polina <polina@mail.ru>
2022-07-12 20:49:54 +0300

    add layout folder,header,main,footer,ui component

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,56 @@
 import React from 'react';
 import './App.css';
+import Button from './components/generic(ui)/buttons/Button';
+import Input from './components/generic(ui)/inputs/Input';
+import Logo from './components/generic(ui)/logo/Logo';
 
 function App() {
   return (
     <div className="App">
-  
+  <Button></Button>
+  <Input></Input>
+  <Logo></Logo>
     </div>
   );
 }
 
-const string: string = 'String';
-const number: string = '777';
+// const string: string = 'String';
+// const number: string = '777';
 
-function stringFunction (string1: string,string2: string): string {
-   return  string1  + string2
-}
-
-
-const result: string = stringFunction(string,number)
-
-interface Card {
-  title: string;
-  rating: number;
-  poster: string;
-}
-
-const card: Card = {title:'Hello', rating: 7.1,poster: 'src'}
+// function stringFunction (string1: string,string2: string): string {
+//    return  string1  + string2
+// }
 
 
+//const result: string = stringFunction(string,number)
 
-interface User {
-     id: number;
-     info: UserInfo;
-     admin?: boolean;
-}
+// interface Card {
+//   title: string;
+//   rating: number;
+//   poster: string;
+// }
 
-interface UserInfo {
-  age: number;
-  name: string;
-  location: string;
-}
+//const card: Card = {title:'Hello', rating: 7.1,poster: 'src'}
 
 
-const user1:User = {id:12244,info:{
-  age:22,
-  name:'Bob',
-  location:'minsk'
-}}
+
+// interface User {
+//      id: number;
+//      info: UserInfo;
+//      admin?: boolean;
+// }
+
+// interface UserInfo {
+//   age: number;
+//   name: string;
+//   location: string;
+// }
+
+
+// const user1:User = {id:12244,info:{
+//   age:22,
+//   name:'Bob',
+//   location:'minsk'
+// }}
 
 export default App;
---
+++ src/components/generic(ui)/inputs/Input.tsx
@@ -0,0 +1,12 @@
+import "./Input.css"
+
+const Input = () => {
+    return (
+     <div className="input">
+        <input type='search' >
+            
+        </input>
+     </div>
+    )
+}
+export default Input
